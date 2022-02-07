4b0b95d5919e9e4a789402331ee7c9554074e3b9
hvalmer <hvalmer@gmail.com>
2022-02-07 17:22:35 -0300

    Handling exceptions and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -16,21 +18,31 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		//função para imprimir as peças da partida
 		while(true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			//digitar a posição de origem
-			System.out.print("Source: ");
-			//lendo a posição de origem
-			ChessPosition source = UI.readChessPosition(sc);
-			System.out.println();
-			//digitar a posição de destino
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			//fazendo a chamada
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			
+			try {
+				//chamando o método que limpa a tela de xadrez, a cada vez que voltar no while
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				//digitar a posição de origem
+				System.out.print("Source: ");
+				//lendo a posição de origem
+				ChessPosition source = UI.readChessPosition(sc);
+				System.out.println();
+				//digitar a posição de destino
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				//fazendo a chamada
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch(ChessException ex) {
+				System.out.println(ex.getMessage());//imprime a mensagem para o usuário
+				sc.nextLine();//o programa aguarda eu apertar ENTER
+			}
+			catch(InputMismatchException ex) {
+				System.out.println(ex.getMessage());//imprime a mensagem para o usuário
+				sc.nextLine();//o programa aguarda eu apertar ENTER
+			}
 		}
 	}
-
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -9,10 +9,10 @@ import chess.Color;
 
 public class UI {
 
-	//códigos especiais das cores para imprimir no console
+	// códigos especiais das cores para imprimir no console
 	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
 
-	//cores do texto
+	// cores do texto
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -23,7 +23,7 @@ public class UI {
 	public static final String ANSI_CYAN = "\u001B[36m";
 	public static final String ANSI_WHITE = "\u001B[37m";
 
-	//cores do fundo
+	// cores do fundo
 	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
 	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
 	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
@@ -33,20 +33,27 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
-	//método para ler(read) uma posição da peça
+	//método para limpar a tela quando da movimentação no xadrez
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
+	// método para ler(read) uma posição da peça
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String st = sc.nextLine();
 			char column = st.charAt(0);
-			//recortar o string a partir da posição 1 e converter(parseInt) para inteiro
+			// recortar o string a partir da posição 1 e converter(parseInt) para inteiro
 			int row = Integer.parseInt(st.substring(1));
 			return new ChessPosition(column, row);
-		}
-		catch(RuntimeException ex) {
+			//lança uma exceção em relação a posição digitada errada InputMismatchException
+		} catch (RuntimeException ex) {
 			throw new InputMismatchException("Error reading ChessPosition. Valid values are form a1 to h8");
 		}
 	}
-	
+
 	// método para imprimir as linhas e colunas
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
@@ -62,16 +69,14 @@ public class UI {
 	// método auxiliar para imprimir uma única peça
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
-            System.out.print("-" + ANSI_RESET);
-        }
-        else {
-            if (piece.getColor() == Color.WHITE) {
-                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-            }
-            else {
-                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
-            }
-        }
-        System.out.print(" ");
+			System.out.print("-" + ANSI_RESET);
+		} else {
+			if (piece.getColor() == Color.WHITE) {
+				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+			} else {
+				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+			}
+		}
+		System.out.print(" ");
 	}
 }
